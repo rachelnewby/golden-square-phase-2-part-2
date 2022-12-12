b88498b19e2ae6c163abaeebcbd31ac87ad79063
Finn McCoy <finnmccoy99@gmail.com>
2022-12-12 16:02:44 +0000

    Wrote two tests which test #mark_done and #done

--- phase_two_tdd/todo_multi_class_program/spec/todo_spec.rb
+++ phase_two_tdd/todo_multi_class_program/spec/todo_spec.rb
@@ -6,7 +6,20 @@ describe Todo do
       chore_1 = Todo.new("make bed")
       expect(chore_1.task).to eq "make bed"
     end
+    
+    it "#done? returns false" do
+      expect(Todo.new("make bed").done?).to eq false
+    end
   end
+  
+  context "After #mark_done! is called" do
+    it "#done? returns true" do
+      todo = Todo.new("make bed")
+      todo.mark_done!
+      expect(todo.done?).to eq true
+    end
+  end
+  
 end
 
 
